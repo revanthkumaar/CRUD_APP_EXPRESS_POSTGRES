34617502479189aaac91498ed967f0d8d9d8f34a
revanthkumaar <iamrevanthkumar@gmail.com>
2021-07-04 11:25:40 +0530

    create operation

--- app/controllers/tutorial.controller.js
+++ app/controllers/tutorial.controller.js
@@ -6,6 +6,31 @@ const Tutorial = db.tutorials;
 const Op = db.Sequelize.Op;
 // Create and Save a new Tutorial
 exports.create = (req, res) => {
+    //validate request
+    if (!req.body.title) {
+        res.status(400).send({
+            message: 'Content is empty'
+        });
+        return;
+    }
+
+    //create the data using model
+    //1. create an object
+    const tutorial = {
+        title: req.body.title,
+        description: req.body.description,
+        published: req.body.published ? req.body.published : false
+    }
+    //2. save it in database
+    Tutorial.create(tutorial)
+        .then(data => {
+            res.send(data);
+        })
+        .catch(err => {
+            res.status(500).send({
+                message: err.message || "Some error occurred while creating the Tutorial."
+            });
+        });
 
 };
 
